ef0935114440264a93d65af1e0fba499b29ab5e4
duy <duy211099@gmail.com>
2023-09-18 10:28:09 +0000

    change to sqlite

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
 export const sequelize = new Sequelize(process.env.DB, process.env.USERNAME, process.env.PASSWORD, {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-        keepAlive: true,
-    },
-    logging: false,
+    // host: process.env.HOST,
+    // port: process.env.PORT,
+    // dialect: 'postgres',
+    // dialectOptions: {
+    //     keepAlive: true,
+    // },
+    // logging: false,
     // SQLite only
-    // storage: 'database.sqlite',
+    dialect: 'sqlite',
+    storage: 'database.sqlite',
 });
 
 export const Channels = sequelize.define('channels', {
